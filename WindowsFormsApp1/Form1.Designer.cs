62281cdffe5b1f36ee7faac4ec2c1530766955c1
joelee2017 <as955133@gmail.com>
2017-11-29 10:32:11 +0800

    上傳

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.myNumericTextBox2 = new WindowsFormsControlLibrary1.MyNumericTextBox();
+            this.myNumericTextBox1 = new WindowsFormsControlLibrary1.MyNumericTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MyNumericeTextBox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MyNumericeTextBox2";
+            // 
+            // myNumericTextBox2
+            // 
+            this.myNumericTextBox2.Location = new System.Drawing.Point(143, 111);
+            this.myNumericTextBox2.Name = "myNumericTextBox2";
+            this.myNumericTextBox2.P1 = 100;
+            this.myNumericTextBox2.P2 = 0;
+            this.myNumericTextBox2.Size = new System.Drawing.Size(100, 22);
+            this.myNumericTextBox2.TabIndex = 2;
+            // 
+            // myNumericTextBox1
+            // 
+            this.myNumericTextBox1.Location = new System.Drawing.Point(143, 61);
+            this.myNumericTextBox1.Name = "myNumericTextBox1";
+            this.myNumericTextBox1.P1 = 0;
+            this.myNumericTextBox1.P2 = 0;
+            this.myNumericTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.myNumericTextBox1.TabIndex = 0;
+            this.myNumericTextBox1.Text = "777";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 319);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.myNumericTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.myNumericTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WindowsFormsControlLibrary1.MyNumericTextBox myNumericTextBox1;
+        private System.Windows.Forms.Button button1;
+        private WindowsFormsControlLibrary1.MyNumericTextBox myNumericTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsControlLibrary1/MyNumericTextBox.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary1
+{
+   public class MyNumericTextBox : TextBox
+    {
+        public int P1 { get; set; }//自動實作
+
+        private int m_P2; //實作欄位
+        public int P2 //實作屬性
+        {
+            get
+            {
+                //............
+                return m_P2;
+            }
+            set
+            {
+                //..............
+                m_P2 = value;
+            }
+        }
+
+
+        public override string Text //將屬性設為 只能數值
+        //{ get => base.Text; set => base.Text = value; }
+        {
+            get
+            {
+                return base.Text;
+            }
+            set
+            {
+                int result;
+                if(int.TryParse(value, out result))//如果不是value 就out排除
+                {
+                    base.Text = value;
+                }
+            }
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)//keyWord事件
+        {// 程式執行時的keyWord事件。
+            if (e.KeyChar ==8)//8代表鍵盤Backspace
+            {
+                return;
+            }
+            if(e.KeyChar<'0' || e.KeyChar >'9')
+            {
+                e.Handled = true;  //表示e 處理過就不往下傳了
+            }
+        }
+
+
+    }
+}
